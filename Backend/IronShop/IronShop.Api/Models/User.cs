2648381f899bcd7a6b5904d1b4f9e26867cf5b42
Morales Franco <franco.e.morales@gmail.com>
2018-12-04 22:55:32 -0300

    Add backend

---
+++ Backend/IronShop/IronShop.Api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IronShop.Api.Models
+{
+    public class User
+    {
+        [BsonElement("fullName")]
+        public string FullName { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+
+        [BsonElement("role")]
+        public string Role { get; set; }
+    }
+}
