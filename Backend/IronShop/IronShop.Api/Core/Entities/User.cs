f2736b71f8ced7b165549dedcee50f23cb0330d3
Morales Franco <franco.e.morales@gmail.com>
2019-01-27 22:20:06 -0300

    Advance backend

--- Backend/IronShop/IronShop.Api/Core/Entities/User.cs
+++ Backend/IronShop/IronShop.Api/Core/Entities/User.cs
@@ -1,23 +1,18 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IronShop.Api.Models
+namespace IronShop.Api.Core.Entities
 {
     public class User
     {
-        [BsonElement("fullName")]
         public string FullName { get; set; }
 
-        [BsonElement("email")]
         public string Email { get; set; }
 
-        [BsonElement("password")]
         public string Password { get; set; }
 
-        [BsonElement("role")]
         public string Role { get; set; }
     }
 }
--- Backend/IronShop/IronShop.Api/Core/IUnitOfWork.cs
+++ Backend/IronShop/IronShop.Api/Core/IUnitOfWork.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IronShop.Api.Dtos
+namespace IronShop.Api.Core
 {
-    public class UserDto
+    interface IUnitOfWork
     {
-
     }
 }
